b308a0865e6581e7dee4a5db0bde8980f48b4250
joanamarta <joanamartasouza@gmail.com>
2025-10-14 16:57:19 -0300

    10/10/2025

--- crud_produto/src/main/java/com/senai/crud_produto/interface_ui_controller/ProdutoController.java
+++ crud_produto/src/main/java/com/senai/crud_produto/interface_ui_controller/ProdutoController.java
@@ -11,31 +11,46 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/api/produtos")
+@RequestMapping("/api/produtos")
 @RequiredArgsConstructor
 public class ProdutoController {
     private final ProdutoService service;
 
 
-
     @PostMapping
-    public ResponseEntity<ProdutoDTO.ProdutoResponse> cadastrarProduto(){
+    public ResponseEntity<ProdutoDTO.ProdutoResponse> cadastrarProduto() {
         Produto produtoCadastrado = service.cadastrarProduto();
-        return ResponseEntity.created(URI.create("/api/produtos"+produtoCadastrado.getId()));
+        return ResponseEntity.created(URI.create("/api/produtos" + produtoCadastrado.getId()));
         .body(ProdutoDTO.ProdutoResponse);
     }
-   @GetMapping
-    public  ResponseEntity<ProdutoDTO.ProdutoResponse>cadastrarProduto(@RequestBody ProdutoDTO.ProdutoRequest dto){
+
+    @GetMapping
+    public ResponseEntity<ProdutoDTO.ProdutoResponse> cadastrarProduto(@RequestBody ProdutoDTO.ProdutoRequest dto) {
         ProdutoDTO.ProdutoResponse.produtoCadastrado = service.cadastrarProduto(dto);
-        return ResponseEntity.created(URI.create("/api/produtos"+produtoCadastrado.id()))
+        return ResponseEntity.created(URI.create("/api/produtos" + produtoCadastrado.id()))
                 .body(produtoCadastrado);
-   }
-   @GetMapping
-    public ResponseEntity<List ProdutoDTO.ProdutoDTO.ProdutoResponse>>listarProdutos(){
+    }
+
+    @GetMapping
+    public ResponseEntity<List ProdutoDTO.ProdutoDTO.ProdutoResponse>>
+
+    listarProdutos() {
         return ResponseEntity.ok(service.listarProdutos());
     }
+
     @GetMapping("/{id}")
-        public ResponseEntity<ProdutoDTO.ProdutoResponse>buscarPorId(@PathVariable String id){
+    public ResponseEntity<ProdutoDTO.ProdutoResponse> buscarPorId(@PathVariable String id) {
         return ResponseEntity.ok(service.buscarPorId(id));
-        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoDTO.ProdutoResponse> atualizarProduto(@PathVariable String id, @RequestBody ProdutoDTO.ProdutoRequest dto)
+        return ResponseEntity.ok(service.atualizarProduto(id,dto));
+
+   }
+   @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable String id) {
+    service.deletarProduto(id);
+    return ResponseEntity.noContent().build();
 }
+
--- crud_produto/src/main/java/com/senai/crud_produto/service/ProdutoService.java
+++ crud_produto/src/main/java/com/senai/crud_produto/service/ProdutoService.java
@@ -1,11 +1,20 @@
 package com.senai.crud_produto.service;
 
 import com.senai.crud_produto.dto.ProdutoDTO;
+import com.senai.crud_produto.entity.Alimentos;
+import com.senai.crud_produto.entity.Limpeza;
 import com.senai.crud_produto.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+
+import static sun.awt.image.MultiResolutionCachedImage.map;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +23,7 @@ public class ProdutoService {
     private  final ProdutoRepository repository;
     public ProdutoDTO.ProdutoResponse cadastrarProduto(ProdutoDTO.ProdutoRequest dto) {
 
-        return ProdutoDTO.ProdutoResponse.fromEntity{
+        return ProdutoDTO.ProdutoResponse.fromEntity(){
             repository.save(dto.toEntity())
         };
     }
@@ -29,4 +38,21 @@ public class ProdutoService {
                 .map(ProdutoDTO.ProdutoResponse::fromEntity)
                 .orElseThrow(()-> new RuntimeException("Produto não encontrado"));
     }
+   public ProdutoDTO.ProdutoResponse atualizarProduto(String id, ProdutoDTO.ProdutoResponse dto){
+       .map(produto -> {)
+           produto.setNome(dto.nome());
+           produto.setMarca(dto.marca());
+           produto.setPreco(dto.preco());
+           produto.setQuantidade(dto.quantidade());
+           if (produto instanceof Limpeza limpeza){
+                limpeza.setPerfume(dto.perfume());
+           } else if (produto instanceof Alimentos alimentos) {
+               alimentos.setPerecivel(dto.perecivel());
+
+           }
+           return ProdutoDTO.ProdutoResponse.fromEntity(repository.save(produto));
+       })
+               .orElseThrow(()-> new RuntimeException("Produto não encontrado"));
+    }
 }
+
